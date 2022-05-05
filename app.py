6ef05fd503ce6b6c57f4d78596eab33fe620cae3
oni-97 <oni.ils.thou@gmail.com>
2022-05-05 22:29:06 +0900

    chart elements

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("This is a title")
 st.write("Hello **World**!")
@@ -32,3 +33,8 @@ st.json(
         ],
     }
 )
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+st.line_chart(chart_data)
+st.area_chart(chart_data)
+st.bar_chart(chart_data)
