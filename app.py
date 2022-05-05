57d768909eceeb0a19b30d200f4072f8f54ffb39
oni-97 <oni.ils.thou@gmail.com>
2022-05-05 22:38:41 +0900

    input widgets

--- app.py
+++ app.py
@@ -38,3 +38,21 @@ chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 st.line_chart(chart_data)
 st.area_chart(chart_data)
 st.bar_chart(chart_data)
+
+if st.button("Click me"):
+    st.write("Thanks to click")
+
+agree = st.checkbox("I agree")
+if agree:
+    st.write("Great!")
+
+options = st.multiselect(
+    "What are your favorite colors",
+    ["Green", "Yellow", "Red", "Blue"],
+    ["Yellow", "Red"],
+)
+st.write("You selected:", options)
+
+
+age = st.slider("How old are you?", 0, 130, 20)
+st.write("I'm ", age, "years old")
